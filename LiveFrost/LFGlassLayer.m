273f55e89ea8e9c184caa0246e224b58552ef26f
Nicholas Levin <nglevin@vivarium.gs>
2014-05-20 15:54:15 -0700

    Missed this one when updating CONTRIBUTIONS

--- LiveFrost/LFGlassLayer.m
+++ LiveFrost/LFGlassLayer.m
@@ -19,8 +19,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 //
-// Contains contributions from Nam Kennic
-//
 
 #import "LFGlassLayer.h"
 #import "LFDefines.h"
